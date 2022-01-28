8ff24179926681380a9254afa77deb58bda5e3de
brawl-kyan <79627900+brawl-kyan@users.noreply.github.com>
2022-01-28 12:51:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function startClassify(){
+navigator.mediaDevices.getUserMedia({ audio: true});
+classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/VgzN33qXH/model.json', modelReady) 
+
+}
+
+function modelReady(){
+classifier.classify(gotResults);
+}
+
+function gotResults(){
+    
+}
